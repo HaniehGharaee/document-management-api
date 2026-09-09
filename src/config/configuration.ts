89d504e85383382ddd33be4004dbe37eefecb531
HaniehGharaee <gharaie.hanieh@gmail.com>
2026-09-09 16:16:51 +0330

    Setting up database config

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,6 +1,12 @@
+import { readDockerSecret } from 'src/security/readSecrets';
 
 export default () => ({
-    appPort: process.env.PORT || 8081,
-    jwtSecret: process.env.JWT_SECRET,
-    
-})
+  appPort: process.env.PORT || 8081,
+  jwtSecret: process.env.JWT_SECRET,
+  mongodb: {
+    uri: process.env.MONGODB_URI,
+    dbName: process.env.MONGODB_DBNAME,
+    userName: process.env.MONGODB_USERNAME,
+    password: readDockerSecret('secretName') || process.env.MONGODB_PASSWORD,
+  },
+});
