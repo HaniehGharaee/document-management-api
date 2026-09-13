184fc7379e71b226a82877a15278d987d80c832f
HaniehGharaee <gharaie.hanieh@gmail.com>
2026-09-13 15:46:05 +0330

    feat: configure NestJS application bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  // Get ConfigService from the DI container.
+  const configService = app.get(ConfigService);
+  const port = configService.getOrThrow<number>('appPort');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Strip properties that do not have any decorators
+      transform: true, // Automatically transform payloads to be objects typed according to their DTO classes
+      forbidNonWhitelisted: true, // Throw an error if non-whitelisted properties are present
+    }),
+  );
+  app.enableCors({
+    origin: 'http://localhost:3000', // Allow requests from the frontend origin
+    credentials: true, // Allow cookies and other credentials in cross-origin requests
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'], // Allow these HTTP methods for cross-origin requests
+  });
+  await app.listen(port);
 }
 bootstrap();
